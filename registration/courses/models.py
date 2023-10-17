a36fcc84e1c92c1fda0172696bdcfa6eb96449f2
Kittisak Suddaen <6410685082@cn330>
2023-10-17 13:42:59 +0700

    comment unuse string format

--- registration/courses/models.py
+++ registration/courses/models.py
@@ -9,12 +9,12 @@ class Course(models.Model):
     year = models.CharField(max_length=8)
     quota = models.IntegerField(default=40)
     open = models.BooleanField(default=True)
-    def __str__(self):
-        return f"{self.name} ({self.num}) : {self.quota} - {self.sem}/{self.year}"
+    # def __str__(self):
+    #     return f"{self.name} ({self.num}) : {self.quota} - {self.sem}/{self.year}"
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     courses = models.ManyToManyField(Course, blank=True, related_name='students')
-    def __str__(self):
-        return f"{self.user} : {self.courses.name}"
+    # def __str__(self):
+    #     return f"{self.user} : {self.courses.name}"
     
