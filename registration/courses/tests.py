51291c4aad73d71fab0831a95773f3909d222b7a
Kittisak Suddaen <6410685082@cn330>
2023-10-17 13:44:03 +0700

    finished all testcase (no miss)

--- registration/courses/tests.py
+++ registration/courses/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Course, Student
 
 # Create your tests here.
+class CourseTestCase(TestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create_superuser(username='admin', password='admin', email='admin@test.com')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.course1 = Course.objects.create(name='Course 1', quota=5)
+        self.course2 = Course.objects.create(name='Course 2', quota=3)
+
+    def test_register_course_view(self):
+        client = Client()
+        client.login(username='testuser', password='testpassword')
+        response = client.post(f'/courses/register/{self.course1.id}/')
+        self.assertRedirects(response, '/courses/enrollment')
+        self.assertTrue(Student.objects.filter(user=self.user, courses=self.course1).exists())
+        self.assertEqual(Course.objects.get(id=self.course1.id).quota, 4)
+
+    def test_cancel_registration_view(self):
+        client = Client()
+        client.login(username='testuser', password='testpassword')
+        student = Student.objects.create(user=self.user)
+        response = client.post(f'/courses/register/{self.course2.id}/')
+        self.assertRedirects(response, '/courses/enrollment')
+        response = client.post(f'/courses/cancel/{self.course2.id}/{student.id}/')
+        self.assertRedirects(response, '/courses/mycourses')
+        self.assertFalse(Student.objects.filter(user=self.user, courses=self.course2).exists())
+        self.assertEqual(Course.objects.get(id=self.course2.id).quota, 3)
+
+    def test_mycourses_view(self):
+        client = Client()
+        client.login(username='testuser', password='testpassword')
+        student = Student.objects.create(user=self.user)
+        student.courses.add(self.course1)
+        response = client.get('/courses/mycourses')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Course 1')
+        
+    def test_admin_check(self):
+        client = Client()
+        client.login(username='testuser', password='testpassword')
+        student = Student.objects.create(user=self.user)
+        student.courses.add(self.course2)
+        client.login(username='admin', password='admin')
+        response = client.get('/courses/admin_check')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(Student.objects.filter(user=self.user).exists())
+        self.assertTrue(Student.objects.filter(user=self.user, courses=self.course2).exists())
--- registration/users/tests.py
+++ registration/users/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        
+    def test_pass_login(self):
+        client = Client()
+        data = {'username': 'testuser', 'password': 'testpassword'}
+        response = client.post('/users/login', data = data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/users/')
+        
+    def test_wrong_login(self):
+        client = Client()
+        data = {'username': 'amumunu', 'password': 'password123'}
+        response = client.post('/users/login', data = data)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_login_page(self):
+        client = Client()
+        response = client.get('/users/login')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_logout(self):
+        client = Client()
+        client.login(username='testuser', password='testpassword')
+        response = client.post('/users/logout')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_pass_main(self):
+        client = Client()
+        client.login(username='testuser', password='testpassword')
+        response = client.get('/users/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_without_login_main(self):
+        client = Client()
+        response = client.get('/users/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/users/login')
